068fa9ed237afa8d0de9c6aa04e864ec7e7a6fe9
Brugha Fitzpatrick <brughafitzpatrick@gmail.com>
2024-06-21 18:32:39 +0100

    Added Upcoming movies page.

---
+++ src/pages/upcomingMoviesPage.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import { BaseMovieProps } from "../types/interfaces";
+import PageTemplate from '../components/templateMovieListPage';
+
+const UpcomingMoviesPage: React.FC = () => {
+    const [movies, setMovies] = useState<BaseMovieProps[]>([]);
+    const favourites = movies.filter(m => m.favourite)
+    localStorage.setItem('favourites', JSON.stringify(favourites))
+    // New function
+    const addToFavourites = (movieId: number) => {
+        const updatedMovies = movies.map((m: BaseMovieProps) =>
+        m.id === movieId ? { ...m, favourite: true } : m
+        );
+        setMovies(updatedMovies);
+    };
+    useEffect(() => {
+        getUpcomingMovies().then(movies => {
+            setMovies(movies);
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return (
+        <PageTemplate
+            title='Discover Movies'
+            movies={movies}
+            selectFavourite={addToFavourites}
+        />
+    )
+};
+
+export default UpcomingMoviesPage
