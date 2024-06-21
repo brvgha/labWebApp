1959a0577c73410b25bd5e3a95e82c4b93f17393
Brugha Fitzpatrick <brughafitzpatrick@gmail.com>
2024-06-21 17:22:02 +0100

    following lab directions

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,19 +1,19 @@
 export interface BaseMovieProps {
-    title: string;
-    budget: number;
-    homepage: string | undefined;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    popularity: number;
-    poster_path?: string;
-    tagline: string;
-    runtime: number;
-    revenue: number;
-    vote_count: number;
+  title: string;
+  budget: number;
+  homepage: string | undefined;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  poster_path?: string;
+  tagline: string;
+  runtime: number;
+  revenue: number;
+  vote_count: number;
   favourite?: boolean;
   genre_ids?: number[];
 }
@@ -31,7 +31,7 @@ export interface MovieDetailsProps extends BaseMovieProps {
   production_countries: {
       iso_3166_1: string;
       name: string;
-    }[];
+  }[];
 }
 
 export interface MovieImage {
@@ -53,4 +53,10 @@ export type FilterOption = "title" | "genre";
 
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
   title: string;
+}
+
+export interface Review{
+  id: string;
+  content: string
+  author: string
 }
